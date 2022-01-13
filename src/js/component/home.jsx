5d1dc163a29358ea89c258287fbf7f22ea40e5f0
julio maldonado <jucemalco@gmail.com>
2022-01-13 08:47:14 -0300

    first update Traffic Light By me on React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+export function Home() {
+	const [selectedColor, setSelectedColor] = useState("yellow");
+	//let selectedColor = "yellow";
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-light">
+			<div
+				onClick={() => setSelectedColor("red")}
+				className={
+					"red " + (selectedColor === "red" ? "red-light" : " ")
+				}></div>
+			<div
+				onClick={() => setSelectedColor("yellow")}
+				className={
+					"yellow " +
+					(selectedColor === "yellow" ? "yellow-light" : "")
+				}></div>
+			<div
+				onClick={() => setSelectedColor("green")}
+				className={
+					"green " + (selectedColor === "green" ? "green-light" : "")
+				}></div>
 		</div>
 	);
-};
-
-export default Home;
+}
